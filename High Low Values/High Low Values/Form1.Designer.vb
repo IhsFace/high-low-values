6c39eca1d9fb02f588994df5d128df689297507e
IhsFace <ihsaanifzal75@gmail.com>
2024-03-04 17:02:58 +1100

    Update files

---
+++ High Low Values/High Low Values/Form1.Designer.vb
@@ -0,0 +1,73 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        lstDisplay = New ListBox()
+        btnDisplay = New Button()
+        btnAppend = New Button()
+        SuspendLayout()
+        ' 
+        ' lstDisplay
+        ' 
+        lstDisplay.FormattingEnabled = True
+        lstDisplay.Location = New Point(46, 248)
+        lstDisplay.Name = "lstDisplay"
+        lstDisplay.Size = New Size(721, 164)
+        lstDisplay.TabIndex = 0
+        ' 
+        ' btnDisplay
+        ' 
+        btnDisplay.Location = New Point(207, 174)
+        btnDisplay.Name = "btnDisplay"
+        btnDisplay.Size = New Size(200, 46)
+        btnDisplay.TabIndex = 1
+        btnDisplay.Text = "Display"
+        btnDisplay.UseVisualStyleBackColor = True
+        ' 
+        ' btnAppend
+        ' 
+        btnAppend.Location = New Point(440, 174)
+        btnAppend.Name = "btnAppend"
+        btnAppend.Size = New Size(200, 46)
+        btnAppend.TabIndex = 2
+        btnAppend.Text = "Append Data"
+        btnAppend.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(13F, 32F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(btnAppend)
+        Controls.Add(btnDisplay)
+        Controls.Add(lstDisplay)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents lstDisplay As ListBox
+    Friend WithEvents btnDisplay As Button
+    Friend WithEvents btnAppend As Button
+
+End Class
---
+++ High Low Values/High Low Values/Form1.vb
@@ -0,0 +1,63 @@
+﻿Imports System.IO
+
+Public Class Form1
+    Private Sub btnDisplay_Click(sender As Object, e As EventArgs) Handles btnDisplay.Click
+        Dim strMemberName As String
+        Dim intAgeInYears As Integer
+        Dim dblDonation As Double
+        Dim intGreatestAge As Integer
+        Dim dblHighestDonation As Double
+        Dim dblLowestDonation As Double
+        Dim memberData As StreamReader = File.OpenText("MemberData.txt")
+
+        lstDisplay.Items.Clear()
+
+        If memberData.Peek <> -1 Then
+            memberData.ReadLine()
+            intGreatestAge = memberData.ReadLine()
+            dblDonation = memberData.ReadLine()
+            dblHighestDonation = dblDonation
+            dblLowestDonation = dblDonation
+
+            Do While memberData.Peek <> -1
+                strMemberName = memberData.ReadLine()
+                intAgeInYears = memberData.ReadLine()
+                dblDonation = memberData.ReadLine()
+
+                If intAgeInYears > intGreatestAge Then
+                    intGreatestAge = intAgeInYears
+                End If
+
+                If dblDonation > dblHighestDonation Then
+                    dblHighestDonation = dblDonation
+                ElseIf dblDonation < dblLowestDonation Then
+                    dblLowestDonation = dblDonation
+                End If
+            Loop
+        End If
+
+        lstDisplay.Items.Add("The greatest age is " & intGreatestAge)
+        lstDisplay.Items.Add("The highest donation is " & dblHighestDonation)
+        lstDisplay.Items.Add("The lowest donation is " & dblLowestDonation)
+
+        memberData.Close()
+    End Sub
+
+    Private Sub btnAppend_Click(sender As Object, e As EventArgs) Handles btnAppend.Click
+        Dim strMemberName As String
+        Dim intAgeInYears As Integer
+        Dim dblDonation As Double
+        Dim memberData As StreamWriter = File.AppendText("MemberData.txt")
+
+        strMemberName = InputBox("Enter Name", "Member Name")
+        memberData.WriteLine(strMemberName)
+
+        intAgeInYears = InputBox("Enter Age In Years", "Member Years")
+        memberData.WriteLine(intAgeInYears)
+
+        dblDonation = InputBox("Enter Donation", "Member Donation")
+        memberData.WriteLine(dblDonation)
+
+        memberData.Close()
+    End Sub
+End Class
